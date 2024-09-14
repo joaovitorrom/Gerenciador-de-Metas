11c91221e75219ac4b4019a357ab2e33601739d8
João Vitor <romanijoaovitor@gmail.com>
2024-09-13 22:15:17 -0300

    criação menu

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const start = () => {
+const { select } = require('@inquirer/prompts') // a desestruturação nesse caso serve para extrair apenas a função select.
+
+const start = async () => {
+   
     while(true) {
-        let opcao = "sair";
+         
+        const opcao = await select({
+            message: "> Menu",
+            choices: [
+                {
+                    name: "Cadastar meta",
+                    value: "cadastar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+         });
+
         switch (opcao) {
             case "cadastrar":
                 console.log("Vamos iniciar o cadastro");
                 break
             case "listar":
                 console.log("Vamos listar");
+                break
             case "sair":
                 return
         }
