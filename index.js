d6153a79f8c35b9152a50a6e42b1ab65592dfd90
João Vitor <romanijoaovitor@gmail.com>
2024-09-15 01:13:38 -0300

    lista de metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts') // a desestruturação nesse caso serve para extrair funções do arquivo prompt.
+const { select, input, checkbox } = require('@inquirer/prompts') // a desestruturação nesse caso serve para extrair funções do arquivo prompt.
 
 let metas = [];
 
@@ -16,12 +16,40 @@ const cadastroMeta = async () => {
     })
 }
 
+const listarMeta = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar meta, o espaço marcar/desmarcar e o Enter para finalizar essa procedimento",
+        choices: [...metas]
+    })
+
+    if(respostas.length == 0) {
+        console.log("nenhuma meta selecionada!");
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false;
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+                return m.value == resposta
+            })
+            
+            meta.checked = true;
+        }
+    )
+        
+    console.log("Meta(s) marcada(s) como concluídas");
+
+}
+
 const start = async () => {
    
     while(true) {
          
         const opcao = await select({
-            message: " Menu >",
+            message: "\n Menu >",
             choices: [
                 {
                     name: "Cadastrar meta",
@@ -44,7 +72,7 @@ const start = async () => {
                 console.log(metas);
                 break
             case "listar":
-                console.log("Vamos listar\n");
+                await listarMeta();
                 break
             case "sair":
                 console.log("Até a próxima");
